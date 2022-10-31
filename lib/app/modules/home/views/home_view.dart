047bde39839cbb30d3ef8cdaac4e4b4b3f1f9014
Nadia Lovely <80080969+nadialvy@users.noreply.github.com>
2022-10-31 09:36:20 +0700

    project init

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
